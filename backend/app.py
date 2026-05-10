4245d2f969b3041c1bb8e413dc2b002b4362a7b3
Marcel <marcelaboumatar@gmail.com>
2026-05-10 07:54:10 -0400

    friends and update

--- backend/app.py
+++ backend/app.py
@@ -85,6 +85,23 @@ def login():
         "success": True,
     }), 200
 
+
+@app.route("/delete_user", methods=["POST"])
+def delete_user():
+    email = request.form.get("email")
+
+    if email is None:
+        return jsonify({
+            "success": False,
+            "error": "Missing field"
+        }), 400
+    
+    f.delete_user(email)
+
+    return jsonify({
+        "success": True,
+    }), 200
+
 @app.route("/get_user_info", methods=["POST"])
 def get_user_info():
     email = request.form.get("email")
@@ -154,7 +171,99 @@ def send_friend_request():
     
     return jsonify({
         "success": True,
-        "user": user
+    }), 200
+
+@app.route("/accept_friend_request", methods=["POST"])
+def accept_friend_request():
+
+    email_sender = request.form.get("email_sender")
+    email_receiver = request.form.get("email_receiver")
+
+    if email_sender is None or email_receiver is None:
+        return jsonify({
+            "success": False,
+            "error": "Missing field"
+        }), 400
+    
+    result = f.accept_friend_request(email_sender, email_receiver)
+
+    if result == Status.INVALID_EMAIL:
+        return jsonify({
+            "success": False,
+            "error": "Account Error"
+        }), 409
+    
+    return jsonify({
+        "success": True,
+    }), 200
+
+@app.route("/decline_friend_request", methods=["POST"])
+def decline_friend_request():
+
+    email_sender = request.form.get("email_sender")
+    email_receiver = request.form.get("email_receiver")
+
+    if email_sender is None or email_receiver is None:
+        return jsonify({
+            "success": False,
+            "error": "Missing field"
+        }), 400
+    
+    result = f.decline_friend_request(email_sender, email_receiver)
+
+    if result == Status.INVALID_EMAIL:
+        return jsonify({
+            "success": False,
+            "error": "Account Error"
+        }), 409
+    
+    return jsonify({
+        "success": True,
+    }), 200
+
+@app.route("/remove_friend", methods=["POST"])
+def remove_friend():
+
+    email_user = request.form.get("email_user")
+    email_friend = request.form.get("email_friend")
+
+    if email_user is None or email_friend is None:
+        return jsonify({
+            "success": False,
+            "error": "Missing field"
+        }), 400
+    
+    result = f.remove_friend(email_user, email_friend)
+
+    if result == Status.INVALID_EMAIL:
+        return jsonify({
+            "success": False,
+            "error": "Account Error"
+        }), 409
+    
+    return jsonify({
+        "success": True,
+    }), 200
+
+@app.route("/update_user", methods=["POST"])
+def update_user():
+
+    email = request.form.get("email")
+    new_username = request.form.get("new_username")
+    new_major = request.form.get("new_major")
+    new_age = request.form.get("new_age")
+    new_private_bool = request.form.get("new_private_bool")
+
+    if email is None or new_username is None or new_major is None or new_age is None or new_private_bool is None:
+        return jsonify({
+            "success": False,
+            "error": "Missing field"
+        }), 400
+    
+    f.update_user(email, new_username, new_major, new_age, new_private_bool)
+    
+    return jsonify({
+        "success": True,
     }), 200
 
 if __name__ == "__main__":
---
+++ backend/backend_functions/delete_user.py
@@ -0,0 +1,26 @@
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+from dotenv import load_dotenv
+import os
+
+import hashlib
+import random
+
+from status_enums import Status
+
+def delete_user(email:str):
+    #load the database
+    load_dotenv()
+    DATABASE_URI = os.getenv("DATABASE_URI")
+
+    client = MongoClient(DATABASE_URI, server_api=ServerApi('1'))
+    database = client["user_database"]
+    collection = database["users"]
+
+    collection.delete_one({"email": email})    
+
+    client.close()
+    return Status.SUCCESS
+    
